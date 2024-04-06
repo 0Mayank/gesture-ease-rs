8d2adfdb9537ac069fe1c07270787481cad01eac
0Mayank <1mayanktomar@gmail.com>
2024-04-07 00:48:44 +0530

    ahihega

--- src/camera.rs
+++ src/camera.rs
@@ -20,12 +20,14 @@ use crate::GError;
 #[derive(Debug, Deserialize, Clone, Copy)]
 pub enum Format {
     RGB888,
+    YUYV,
 }
 
 impl Format {
     fn fourcc(&self) -> u32 {
         match self {
             Self::RGB888 => 875710290,
+            Self::YUYV => u32::from_le_bytes([b'Y', b'U', b'Y', b'V']),
         }
     }
 
@@ -36,6 +38,7 @@ impl Format {
     fn planes(&self) -> u32 {
         match self {
             Self::RGB888 => 3,
+            Self::YUYV => 2,
         }
     }
 }
